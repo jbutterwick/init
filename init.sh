c84801dbe2044fa4902d3cd847fbb4cdc03ac9d7
jordan <jordan@butterwick.tech>
2023-06-19 20:50:15 -0600

    readme and init script

---
+++ init.sh
@@ -0,0 +1,6 @@
+dnf install cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++ openssl-devel sqlite-devel python3-tkinter ncurses-devel
+cargo install zellij btm starship rtx-cli bat exa ripgrep
+
+mkdir ~/Projects ~/Dependencies
+
+cd ~/Projects
