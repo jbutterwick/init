f18580eadd7bd2d2ca3843bf28866eaa47b7ebed
jordan <jordan@butterwick.tech>
2023-07-30 20:37:28 -0600

    feat: Add docker installation

--- init.sh
+++ init.sh
@@ -1,6 +1,12 @@
 dnf install cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++ openssl-devel sqlite-devel python3-tkinter ncurses-devel
 cargo install zellij btm starship rtx-cli bat exa ripgrep
 
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
 mkdir ~/Projects ~/Dependencies
 
 cd ~/Projects
