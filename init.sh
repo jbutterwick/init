4361eb960a2b535e64dfb16c146cde7aad71b449
Jordan Butterwick <33608116+jbutterwick@users.noreply.github.com>
2024-09-19 09:22:55 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,14 @@
-dnf install cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++ openssl-devel sqlite-devel python3-tkinter ncurses-devel
+dnf -y install cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++ openssl-devel sqlite-devel python3-tkinter ncurses-devel perl
+dnf -y groupinstall "Development Tools"
+
+# rust and rust packages
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install zellij btm starship rtx-cli bat exa ripgrep
 
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
 mkdir ~/Projects ~/Dependencies
